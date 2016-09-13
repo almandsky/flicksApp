b89015344534deae34ec74bb0cc8034079516c2c
Sky Chen <skychen@yahoo-inc.com>
2016-09-13 06:40:16 -0700

    remove Pods folder

--- Pods/Target Support Files/Pods-flicksApp/Pods-flicksApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flicksApp : NSObject
-@end
-@implementation PodsDummy_Pods_flicksApp
-@end
--- Pods/Target Support Files/Pods-flicksAppUITests/Pods-flicksAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flicksAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_flicksAppUITests
-@end
