cb4767e2e171d55974a442e4dc2d498d93732f68
Sky Chen <skychen@yahoo-inc.com>
2016-09-13 17:55:20 -0700

    add segmented control

---
+++ flicksApp/MovieCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.m
+//  flicksApp
+//
+//  Created by Sky Chen on 9/13/16.
+//  Copyright © 2016 Sky Chen. All rights reserved.
+//
+
+#import "MovieCollectionViewCell.h"
+
+@implementation MovieCollectionViewCell
+
+@end
